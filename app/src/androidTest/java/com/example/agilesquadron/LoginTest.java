062008c6376bc145715d7316a1445aca0c55e0c6
arjudutta123 <arjudutta789@gmail.com>
2020-07-04 21:05:41 +0545

    login test

---
+++ app/src/androidTest/java/com/example/agilesquadron/LoginTest.java
@@ -0,0 +1,30 @@
+package com.example.agilesquadron;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class LoginTest {
+    @Rule
+    public ActivityTestRule<LoginActivity> testRule = new ActivityTestRule<> (LoginActivity.class);
+    String expected ="true";
+
+    @Test
+    public void LoginTest(){
+        onView(withId(R.id.etEmail))
+                .perform(typeText("arjudutta1"))
+                .perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.etpassword)) .perform(typeText("arjudutta11")) .perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.btnlogin)) .perform(click());
+
+    }
+}
