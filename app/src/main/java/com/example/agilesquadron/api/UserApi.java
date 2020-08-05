e2626657e0a53d9776235aa31040f726ed506af5
magarbibek1 <49414069+magarbibek1@users.noreply.github.com>
2020-08-05 15:43:22 +0545

    changes in code

--- app/src/main/java/com/example/agilesquadron/api/UserApi.java
+++ app/src/main/java/com/example/agilesquadron/api/UserApi.java
@@ -1,21 +1,38 @@
 package com.example.agilesquadron.api;
 
+import com.example.agilesquadron.model.UpdateProfile;
 import com.example.agilesquadron.model.User;
+import com.example.agilesquadron.model.username;
 import com.example.agilesquadron.serverresponse.ImageResponse;
 import com.example.agilesquadron.serverresponse.SignUpResponse;
 
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Part;
 
 public interface UserApi {
-    @POST("user/signup")
-    Call<SignUpResponse> registerUser(@Body User user);
 
+
+    @POST("users/signup")
+    Call<SignUpResponse> registerUser(@Body User users);
+
+    @POST("users/login")
+    Call<SignUpResponse> checklogin(@Body username Username);
     @Multipart
     @POST("upload")
     Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+    @GET("users/me")
+    Call<User> getUserDetails(@Header("Authorization")String token);
+    //    @GET("users/me")
+//    Call<Usercrud> getupdate (@Header("Authorization") String token, @Body Usercrud usercrud );
+    @GET("users/images")
+    Call<ImageResponse> getImage(@Part MultipartBody.Part img);
+    @PUT("users/me")
+    Call<UpdateProfile> edituser(@Header("Authorization") String token, @Body UpdateProfile updateProfile);
 }
