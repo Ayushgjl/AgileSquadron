a1bdeb4d8431a905833dad2d5e3c93e60513d2fd
Ayush Gajurel <49242566+Ayushgjl@users.noreply.github.com>
2020-08-11 10:56:02 +0545

    changes in code

--- app/src/main/java/com/example/agilesquadron/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/agilesquadron/ui/home/HomeFragment.java
@@ -126,8 +126,8 @@ public class HomeFragment extends Fragment {
 
         Food();
         Electronic();
-//        Clothe();
-//        Gift();
+        Clothe();
+        Gift();
 
         return root;
     }
@@ -192,61 +192,61 @@ public class HomeFragment extends Fragment {
         });
     }
 
-//    private void Clothe(){
-//        cList = new ArrayList<>();
-//
-//        CApi F = Url.getInstance().create(CApi.class);
-//        Call<List<Clothe>> listCall=F.getClothe();
-//        listCall.enqueue(new Callback<List<Clothe>>() {
-//            @Override
-//            public void onResponse(Call<List<Clothe>> call, Response<List<Clothe>> response) {
-//                if (!response.isSuccessful()){
-//                    Toast.makeText(getContext(), "Error" + response.code(), Toast.LENGTH_SHORT).show();
-//                }
-//
-//                List<Clothe> cList1=response.body();
-//                ClotheAdapter=new ClotheAdapter(getContext(),cList1);
-//                recyclerView_c.setAdapter(ClotheAdapter);
-//                recyclerView_c.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-//                recyclerView_c.setHasFixedSize(true);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Clothe>> call, Throwable t) {
-//
-//                Log.d("Error Message", "Error" + t.getLocalizedMessage());
-//                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//    }
+    private void Clothe(){
+        cList = new ArrayList<>();
 
-//    private void Gift(){
-//        gList = new ArrayList<>();
-//
-//        GApi F = Url.getInstance().create(GApi.class);
-//        Call<List<Gift>> listCall=F.getGift();
-//        listCall.enqueue(new Callback<List<Gift>>() {
-//            @Override
-//            public void onResponse(Call<List<Gift>> call, Response<List<Gift>> response) {
-//                if (!response.isSuccessful()){
-//                    Toast.makeText(getContext(), "Error" + response.code(), Toast.LENGTH_SHORT).show();
-//                }
-//
-//                List<Gift> gList1=response.body();
-//                GiftAdapter=new GiftAdapter(getContext(),gList1);
-//                recyclerView_d.setAdapter(GiftAdapter);
-//                recyclerView_d.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-//                recyclerView_d.setHasFixedSize(true);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Gift>> call, Throwable t) {
-//
-//                Log.d("Error Message", "Error" + t.getLocalizedMessage());
-//                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//    }
+        CApi C = Url.getInstance().create(CApi.class);
+        Call<List<Clothe>> listCall=C.getClothe();
+        listCall.enqueue(new Callback<List<Clothe>>() {
+            @Override
+            public void onResponse(Call<List<Clothe>> call, Response<List<Clothe>> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(getContext(), "Error" + response.code(), Toast.LENGTH_SHORT).show();
+                }
+
+                List<Clothe> cList1=response.body();
+                ClotheAdapter=new ClotheAdapter(getContext(),cList1);
+                recyclerView_d.setAdapter(ClotheAdapter);
+                recyclerView_d.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+                recyclerView_d.setHasFixedSize(true);
+            }
+
+            @Override
+            public void onFailure(Call<List<Clothe>> call, Throwable t) {
+
+                Log.d("Error Message", "Error" + t.getLocalizedMessage());
+                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void Gift(){
+        gList = new ArrayList<>();
+
+        GApi G = Url.getInstance().create(GApi.class);
+        Call<List<Gift>> listCall=G.getGift();
+        listCall.enqueue(new Callback<List<Gift>>() {
+            @Override
+            public void onResponse(Call<List<Gift>> call, Response<List<Gift>> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(getContext(), "Error" + response.code(), Toast.LENGTH_SHORT).show();
+                }
+
+                List<Gift> gList1=response.body();
+                GiftAdapter=new GiftAdapter(getContext(),gList1);
+                recyclerView_c.setAdapter(GiftAdapter);
+                recyclerView_c.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+                recyclerView_c.setHasFixedSize(true);
+            }
+
+            @Override
+            public void onFailure(Call<List<Gift>> call, Throwable t) {
+
+                Log.d("Error Message", "Error" + t.getLocalizedMessage());
+                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
 }
